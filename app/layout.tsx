da319812c52a2c4737fe9c44430354985b5eae77
Innova Space <emorales@colprovidencia.cl>
2026-07-02 20:38:15 -0400

    Cargar estrellas caoticas

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./night-theme.css";
+import "./chaotic-stars.css";
 
 export const metadata: Metadata = {
   title: "Carolina & Esthefano | Invitación de boda",
